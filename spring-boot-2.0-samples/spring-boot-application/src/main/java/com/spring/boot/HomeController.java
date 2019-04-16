09448ac2980e5de600a1c8a18e1a1eea6a0c55ae
Vijaya.mukthala@wipro.com <Vijaya.mukthala@wipro.com>
2019-04-16 17:13:20 +0530

    updated the properties

--- spring-boot-2.0-samples/spring-boot-application/src/main/java/com/spring/boot/HomeController.java
+++ spring-boot-2.0-samples/spring-boot-application/src/main/java/com/spring/boot/HomeController.java
@@ -1,14 +1,20 @@
 package com.spring.boot;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HomeController {
+
+	@Value("${welcome.message}")
+	String welcomeText;
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "first application";
+		return "first application" + welcomeText;
 	}
 
 }
